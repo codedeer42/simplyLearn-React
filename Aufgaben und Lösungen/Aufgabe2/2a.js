bd2c41879cf424f180b94a68f415fb790d15fe8e
codedeer42 <136886721+codedeer42@users.noreply.github.com>
2023-07-01 09:50:14 +0200

    Update 2a.js

--- Aufgaben und Lösungen/Aufgabe2/2a.js
+++ Aufgaben und Lösungen/Aufgabe2/2a.js
@@ -13,7 +13,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-         <Header title="Überschrift" name="Peter"/>
+         <Header title="Überschrift"/>
     </div>
   );
 }
