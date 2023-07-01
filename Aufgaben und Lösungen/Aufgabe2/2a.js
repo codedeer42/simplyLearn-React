1501746d491e27e348eaf4215be78bef89bccf1c
codedeer42 <136886721+codedeer42@users.noreply.github.com>
2023-07-01 09:49:05 +0200

    Update and rename 1a.js to 2a.js

---
+++ Aufgaben und Lösungen/Aufgabe2/2a.js
@@ -0,0 +1,20 @@
+const Header = ({title,name}) => {
+  return (
+     <h1>{title}</h1>
+  )
+}
+
+export default Header
+/*
+import Header from './components/Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+         <Header title="Überschrift" name="Peter"/>
+    </div>
+  );
+}
+*/
